c0ea651b9f97a3939e64d27c9b5d4136c2ecf23d
Raphaël Barrois <raphael.barrois@polytechnique.org>
2013-01-22 00:59:47 +0100

    Add tests.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

---
+++ batchform/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# This code is distributed under the two-clause BSD license.
+# Copyright (c) 2013 Raphaël Barrois
+
+
+from .views import *
---
+++ batchform/tests/views.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+# This code is distributed under the two-clause BSD license.
+# Copyright (c) 2013 Raphaël Barrois
+
+import os.path
+
+
+from django import forms
+from django.test import TestCase
+
+
+data_dir = os.path.join(os.path.dirname(__file__), 'data')
+
+
+class BaseTestCase(TestCase):
+    urls = 'dev.urls'
+
+    def open_data(self, filename, *args, **kwargs):
+        absolute_path = os.path.join(data_dir, filename)
+        return open(absolute_path, *args, **kwargs)
+
+
+class UploadTestCase(BaseTestCase):
+
+    def test_upload_form(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'batchform/upload_form.html')
+
+    def test_uploaded_file(self):
+        with self.open_data('example.csv') as f:
+            response = self.client.post('/', {'file': f, 'global_step': 'upload'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'batchform/lines_form.html')
+        self.assertContains(response, "foo")
+        self.assertContains(response, "bar")
+
+    def test_missing_global_step(self):
+        with self.open_data('example.csv') as f:
+            self.assertRaises(forms.ValidationError, self.client.post,
+                '/', {'file': f})
+
+
+class LinesTestCase(BaseTestCase):
+    def test_lines_form(self):
+        response = self.client.post('/', {
+            'global_step': 'lines',
+            'form-TOTAL_FORMS': '3',
+            'form-INITIAL_FORMS': '3',
+            'form-MAX_NUM_FORMS': '3',
+            'form-0-cola': '1',
+            'form-0-colb': '2',
+            'form-0-colc': '3',
+            'form-1-cola': 'foo',
+            'form-1-colb': 'bar',
+            'form-1-colc': 'baz',
+            'form-2-cola': 'blih',
+            'form-2-colb': 'blah',
+            'form-2-colc': 'bleh',
+        })
+
+        self.assertRedirects(response, '/')
+        self.assertIn(u"3 lines", response.cookies['messages'].value)
+
+    def test_incomplete_form(self):
+        response = self.client.post('/', {
+            'global_step': 'lines',
+            'form-TOTAL_FORMS': '3',
+            'form-INITIAL_FORMS': '3',
+            'form-MAX_NUM_FORMS': '3',
+            'form-0-cola': '1',
+            'form-0-colb': '2',
+            'form-0-colc': '3',
+            'form-1-cola': 'foo',
+            'form-1-colb': 'bar',
+            'form-1-colc': 'baz',
+            'form-2-cola': 'blih',
+            'form-2-colb': '',  # Missing required field
+            'form-2-colc': 'bleh',
+        })
+
+        self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed(response, 'batchform/lines_form.html')
+        self.assertContains(response, 'foo')
+        self.assertContains(response, 'bar')
+        self.assertContains(response, 'blih')
+        self.assertContains(response, u"required")
+
+    def test_lines_form_with_deleted(self):
+        response = self.client.post('/', {
+            'global_step': 'lines',
+            'form-TOTAL_FORMS': '3',
+            'form-INITIAL_FORMS': '3',
+            'form-MAX_NUM_FORMS': '3',
+            'form-0-cola': '1',
+            'form-0-colb': '2',
+            'form-0-colc': '3',
+            'form-1-cola': 'foo',
+            'form-1-colb': 'bar',
+            'form-1-colc': 'baz',
+            'form-1-DELETE': 'on',
+            'form-2-cola': 'blih',
+            'form-2-colb': 'blah',
+            'form-2-colc': 'bleh',
+        })
+
+        self.assertRedirects(response, '/')
+        self.assertIn(u"2 lines", response.cookies['messages'].value)
